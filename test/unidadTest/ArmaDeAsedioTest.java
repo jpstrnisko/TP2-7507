b165a408cf8fb7fb20069c1d7aed1af9a0a117b9
Lautis1 <lautaro.stroia@gmail.com>
2018-11-10 16:10:17 -0300

    agregadas unidades

---
+++ test/unidadTest/ArmaDeAsedioTest.java
@@ -0,0 +1,20 @@
+package unidadTest;
+import static org.junit.Assert.*;
+import org.junit.Test;
+import modelo.ArmaDeAsedio;
+
+public class ArmaDeAsedioTest {
+
+	@Test
+	public void test01ArmaDeAsedioSeCreaCon150DeVida() {
+		ArmaDeAsedio miArma = new ArmaDeAsedio();
+		assertEquals(150, miArma.obtenerVida());
+	}
+	
+	@Test
+	public void test02ArmaDeAsedioSeCreaConUnCostoDe200() {
+		ArmaDeAsedio miArma = new ArmaDeAsedio();
+		assertEquals(200, miArma.obtenerCosto());
+	}
+
+}
