270174b3d1e0036891dbdcdd54e7fa5ac6eec373
martinsuarezz <44825131+martinsuarezz@users.noreply.github.com>
2018-11-11 21:19:46 -0300

    Prueba de vida

--- test/pruebas/CuartelTest.java
+++ test/pruebas/CuartelTest.java
@@ -11,16 +11,21 @@ import unidades.Espadachin;
 import org.junit.Assert;
 
 public class CuartelTest {
-
-	Cuartel cuartel = new Cuartel();
-			
 	@Test
 	public void elCuartelCreaUnEspadachin() {
+		Cuartel cuartel = new Cuartel();
 		Assert.assertTrue(cuartel.crearUnEspadachin() instanceof Espadachin);
 	}
 	
+	@Test
 	public void elCuartelCreaUnArquero() {
+		Cuartel cuartel = new Cuartel();
 		Assert.assertTrue(cuartel.crearUnArquero() instanceof Arquero);
 	}
-
+	
+	@Test
+	public void elCuartelSeCreaCon250DeVida() {
+		Cuartel cuartel = new Cuartel();
+		Assert.assertEquals(cuartel.obtenerVida(), 250);
+	}
 }
