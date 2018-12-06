2634fa67a9f6864476aae9c93aa6a65f5761e1fc
martinsuarezz <44825131+martinsuarezz@users.noreply.github.com>
2018-12-06 11:16:30 -0300

    cambio en pruebas ya chequeadas en integracion

--- test/edificiosTest/CastilloTest.java
+++ test/edificiosTest/CastilloTest.java
@@ -42,56 +42,6 @@ public class CastilloTest {
 		assertEquals(castillo.obtenerVida(), 1000);
 	}
 	
-	@Test 
-	public void alAtacarAcualquierUnidadDentroDelRangoLeResta20DeVida() {
-		Castillo castillo = new Castillo();
-		Aldeano aldeano = new Aldeano();
-		aldeano.establecerPosicion(new Posicion(7, 7));
-		castillo.establecerPosicion(new Posicion(1, 1));
-		int vidaActual = aldeano.obtenerVida();
-		castillo.atacar(aldeano);
-		assertEquals(vidaActual - 20, aldeano.obtenerVida());
-	}
-	
-	@Test
-	public void alAtacarACualquierEdificioLeResta20DeVida() {
-		Castillo castillo = new Castillo();
-		Cuartel cuartel = new Cuartel();
-		cuartel.establecerPosicion(new Posicion(1, 1));
-		castillo.establecerPosicion(new Posicion(4, 4));
-		int vidaActual = cuartel.obtenerVida();
-		castillo.atacar(cuartel);
-		assertEquals(vidaActual - 20, cuartel.obtenerVida());
-	}
-	
-	@Test
-	public void elCastilloNoDeberiaPoderAtacarAUnEdificioFueraDeSuRango() {
-		Castillo castillo = new Castillo();
-		Cuartel cuartel = new Cuartel();
-		cuartel.establecerPosicion(new Posicion(1, 1));
-		castillo.establecerPosicion(new Posicion(4, 4));
-		int vidaActual = cuartel.obtenerVida();
-		castillo.atacar(cuartel);
-		assertEquals(vidaActual - 20, cuartel.obtenerVida());
-	}
-	
-	@Test
-	public void elCastilloNoDeberiaPoderAtacarUnidadesFueraDeSuRango() {
-		Castillo castillo = new Castillo();
-		Aldeano aldeano1 = new Aldeano();
-		Aldeano aldeano2 = new Aldeano();
-		aldeano1.establecerPosicion(new Posicion(1, 8));
-		aldeano2.establecerPosicion(new Posicion(8, 8));
-		castillo.establecerPosicion(new Posicion(1, 1));
-		int vidaActual1 = aldeano1.obtenerVida();
-		int vidaActual2 = aldeano2.obtenerVida();
-		
-		castillo.atacar(aldeano1);
-		castillo.atacar(aldeano2);
-		assertEquals(vidaActual1, aldeano1.obtenerVida());
-		assertEquals(vidaActual2, aldeano2.obtenerVida());
-	}
-	
 	
 	
 	
